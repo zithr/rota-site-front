91e03f2a449db4795ff19555298fa75eb2da7d1d
zithr <zither.sorrow@gmail.com>
2022-04-07 22:24:45 +0100

    update example rota

--- src/components/RotaTable.svelte
+++ src/components/RotaTable.svelte
@@ -1,64 +1,85 @@
 <script>
-    import { onMount } from 'svelte'
+    import { onMount } from "svelte"
     import ShiftCard from "./ShiftCard.svelte"
     import InteractMenu from "./rota_menu/InteractMenu.svelte"
-    import { RotaStore } from "../stores";
-    import { VolStore } from "../stores";
-    import { MenuInfoStore } from "../stores";
-    import { DateStore } from "../stores";
-    import { obj } from "../test_rota_old.json"
+    import { RotaStore } from "../stores"
+    import { VolStore } from "../stores"
+    import { MenuInfoStore } from "../stores"
+    import { DateStore } from "../stores"
+    import { obj } from "../test_rota.json"
     import { vols_list } from "../test_vols.json"
     import plus from "../svg/plus.svelte"
-    import { Circle2 } from 'svelte-loading-spinners'
-import Plus from '../svg/plus.svelte';
+    import { Circle2 } from "svelte-loading-spinners"
+    import Plus from "../svg/plus.svelte"
     // const shifts_list = obj["Shifts"]
     // let shifts_list = $RotaStore["Shifts"]
     const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
     const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
     $: display_start = []
-    
+
     let combo_shifts = []
 
     // load initial rota
     // TODO load active vol list
     onMount(async () => {
-        let temp_shifts
         // let temp_shifts = obj["Shifts"]  // load from py instead of json file
-        let res = await fetch("https://3.90.102.33/api/rota", { // https://3.90.102.33  // 
-                method: "GET",
-                mode: "cors",
-                // Allows cookies to be set/accessed
-                credentials: "include",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-            }).then(x => x.json()).then(data => { if (!data) {
-                // need to dispatch login request + last fetch request to queue up after login success
+        let res = await fetch("https://3.90.102.33/api/rota", {
+            // https://3.90.102.33  //
+            method: "GET",
+            mode: "cors",
+            // Allows cookies to be set/accessed
+            credentials: "include",
+            headers: {
+                "Content-Type": "application/json",
+            },
+        })
+            .then((x) => x.json())
+            .then((data) => {
+                if (!data) {
+                    // need to dispatch login request + last fetch request to queue up after login success
+                    console.log("login error, no data found, using fake data")
+                    temp_shifts = obj["Shifts"]
+                    $RotaStore["Shifts"] = temp_to_combo(temp_shifts)
+                    visible_shifts = findStartDates()
+                } else {
+                    temp_shifts = data
+                    combo_shifts = temp_to_combo(data)
+                    $RotaStore["Shifts"] = combo_shifts
+                    visible_shifts = findStartDates()
+                }
+            })
+            .catch((error) => {
+                console.log(error)
                 console.log("login error, no data found, using fake data")
-                temp_shifts = obj["Shifts"]
-                $RotaStore["Shifts"] = temp_to_combo(temp_shifts)
-                visible_shifts = findStartDates()
-            }
-        else {
-            temp_shifts = data
-            combo_shifts = temp_to_combo(data)
-            $RotaStore["Shifts"] = combo_shifts
-            visible_shifts = findStartDates()
-        }})
+            })
     })
 
     const temp_to_combo = (temp_shifts) => {
         let combo_shifts = []
-        for (let i=0; i<temp_shifts.length; i++) {
-                if (temp_shifts[i].type === "(Duty Room)" && temp_shifts[i+1].type === "(Leader)" && temp_shifts[i+1].time === temp_shifts[i].time && temp_shifts[i+1].date === temp_shifts[i].date) {
-                        let new_combo = {...temp_shifts[i], "leader_shift_id": temp_shifts[i+1].shift_id, "leader_vols": temp_shifts[i+1].vols, "leader_vol_shift_id": temp_shifts[i+1].vol_shift_id}
-                        combo_shifts.push(new_combo)
-                        i++
-                    }
-                    else {
-                        combo_shifts.push({...temp_shifts[i], "leader_shift_id": null, "leader_vols": null, "leader_vol_shift_id": null})
-                    }
+        for (let i = 0; i < temp_shifts.length; i++) {
+            if (
+                temp_shifts[i].type === "(Duty Room)" &&
+                temp_shifts[i + 1].type === "(Leader)" &&
+                temp_shifts[i + 1].time === temp_shifts[i].time &&
+                temp_shifts[i + 1].date === temp_shifts[i].date
+            ) {
+                let new_combo = {
+                    ...temp_shifts[i],
+                    leader_shift_id: temp_shifts[i + 1].shift_id,
+                    leader_vols: temp_shifts[i + 1].vols,
+                    leader_vol_shift_id: temp_shifts[i + 1].vol_shift_id,
+                }
+                combo_shifts.push(new_combo)
+                i++
+            } else {
+                combo_shifts.push({
+                    ...temp_shifts[i],
+                    leader_shift_id: null,
+                    leader_vols: null,
+                    leader_vol_shift_id: null,
+                })
             }
+        }
         return combo_shifts
     }
 
@@ -66,18 +87,19 @@ import Plus from '../svg/plus.svelte';
         let d = !datetime ? new Date() : datetime
         display_start = []
         d =
-        d.getDay() === 0
-        ? new Date(d.getFullYear(), d.getMonth(), d.getDate() - 6)
-        : new Date(d.getFullYear(), d.getMonth(), d.getDate() - d.getDay() + 1) // set date to a monday
-        
+            d.getDay() === 0
+                ? new Date(d.getFullYear(), d.getMonth(), d.getDate() - 6)
+                : new Date(d.getFullYear(), d.getMonth(), d.getDate() - d.getDay() + 1) // set date to a monday
+
         for (let i = 0; i < 7; i++) {
             let a = new Date(d.getFullYear(), d.getMonth(), d.getDate() + i)
             let a_str = `${a.getDate()} ${months[a.getMonth()]} ${a.getFullYear()}`
             display_start.push(a_str)
         }
         const displayShifts = viewShifts(d)
+        console.log(`findstartdates ${d}`)
         // const newShifts = comboShifts(displayShifts)
-        
+
         return displayShifts
     }
 
@@ -98,33 +120,38 @@ import Plus from '../svg/plus.svelte';
     //     return combo
     // }
 
-    
     //
-    const viewShifts = (start_date = null,) => {
+    let temp_shifts
+    temp_shifts = obj["Shifts"]
+    $RotaStore["Shifts"] = temp_to_combo(temp_shifts)
+    $: visible_shifts = findStartDates()
+
+    const viewShifts = (start_date = null) => {
         let displayShifts = [[], [], [], [], [], [], []]
         let d = new Date()
         if (start_date) {
             d = start_date
         }
-
+        let combo_shifts = $RotaStore["Shifts"]
         const target_start = d
         const target_end = new Date(d.getFullYear(), d.getMonth(), d.getDate() + 7)
         for (const [j, shift] of combo_shifts.entries()) {
             let shift_day = shift.date.slice(0, 2)
             let shift_month = shift.date.slice(3, 6)
             let shift_year = `20${shift.date.slice(7, 9)}`
-            let shift_time = shift.time.slice(0,5).replace(":", "")
+            let shift_time = shift.time.slice(0, 5).replace(":", "")
             for (let i = 0; i < 12; i++) {
                 if (shift_month === months[i]) {
                     shift_month = `${i}`
                 }
             }
 
-            combo_shifts[j].dt_obj = {time:shift_time, day:shift_day, month:shift_month, year:shift_year}  // add dt_obj to shift for easier date ordering
+            combo_shifts[j].dt_obj = { time: shift_time, day: shift_day, month: shift_month, year: shift_year } // add dt_obj to shift for easier date ordering
             let shift_date = new Date(Number(shift_year), Number(shift_month), Number(shift_day))
             if (shift_date >= target_start && shift_date < target_end) {
                 let index = shift_date.getDay() === 0 ? 6 : shift_date.getDay() - 1
                 displayShifts[index].push(shift)
+                console.log("pushing shifts??")
             }
         }
         return displayShifts
@@ -153,6 +180,7 @@ import Plus from '../svg/plus.svelte';
         dt.setDate(dt.getDate() + 7)
         $DateStore = dt
         visible_shifts = findStartDates(dt)
+        console.log(dt)
     }
 
     const rotaPrevious = () => {
@@ -161,7 +189,7 @@ import Plus from '../svg/plus.svelte';
         dt.setDate(dt.getDate() - 7)
         $DateStore = dt
         visible_shifts = findStartDates(dt)
-        console.log(visible_shifts[0])
+        console.log(dt)
     }
 
     const handleAddShift = (date) => {
@@ -173,12 +201,12 @@ import Plus from '../svg/plus.svelte';
         console.log(`add shift on ${dt}`)
     }
 
-    $: visible_shifts = findStartDates()
+    // $: visible_shifts = findStartDates()
     const button_css =
         "p-6 rounded-lg border-gray-600 text-gray-700 font-semibold text-md hover:text-green-900 hover:bg-green-200"
 
     $: showMenu = false
-    let pos = {x:0, y:0}
+    let pos = { x: 0, y: 0 }
     let handleSign = (e) => {
         if ($VolStore.length < 1) {
             //update Volstore
@@ -187,21 +215,19 @@ import Plus from '../svg/plus.svelte';
         $MenuInfoStore = e.detail
 
         // Put box in bounds if not in bounds
-        e.detail.x = Math.min(e.detail.x, window.innerWidth*0.8)
-        if (e.detail.y > window.innerHeight) e.detail.y -= window.innerHeight*0.1
+        e.detail.x = Math.min(e.detail.x, window.innerWidth * 0.8)
+        if (e.detail.y > window.innerHeight) e.detail.y -= window.innerHeight * 0.1
 
-        pos = {x:e.detail.x, y:e.detail.y}
+        pos = { x: e.detail.x, y: e.detail.y }
         showMenu = true
     }
     const handleUpdate = () => {
         console.log("Update")
         visible_shifts = findStartDates($DateStore)
-
     }
-
 </script>
 
-<InteractMenu bind:showMenu={showMenu} {pos} on:rota_updated={handleUpdate}/>
+<InteractMenu bind:showMenu {pos} on:rota_updated={handleUpdate} />
 <div class="min-h-full">
     <div class="flex py-4 justify-center items-center">
         <button on:click={rotaPrevious} class="${button_css} px-3 py-2">Previous</button><button
@@ -212,23 +238,24 @@ import Plus from '../svg/plus.svelte';
 
     <div class="flex justify-center">
         {#if visible_shifts[0].length === 0}
-        <div class="py-24 flex justify-center items-center"><Circle2 size="60" unit="px"></Circle2></div>
+            <div class="py-24 flex justify-center items-center"><Circle2 size="60" unit="px" /></div>
         {:else}
-        {#each visible_shifts as shifts, j}
-            <div class="flex-col py-2 px-6">
-                <div class="font-semibold">
-
-                    <div class="flex justify-center text-green-700 text-lg">
-                        <span on:click={() => handleAddShift(display_start[j])} class="h-4 w-4 cursor-pointer"><svelte:component this={plus} /></span></div>
-                    <div class="flex justify-center items-center">{days[j + 1]}</div>
-                    <div class="flex justify-center items-center text-sm">{display_start[j]}</div>
+            {#each visible_shifts as shifts, j}
+                <div class="flex-col py-2 px-6">
+                    <div class="font-semibold">
+                        <div class="flex justify-center text-green-700 text-lg">
+                            <span on:click={() => handleAddShift(display_start[j])} class="h-4 w-4 cursor-pointer"
+                                ><svelte:component this={plus} /></span
+                            >
+                        </div>
+                        <div class="flex justify-center items-center">{days[j + 1]}</div>
+                        <div class="flex justify-center items-center text-sm">{display_start[j]}</div>
+                    </div>
+                    {#each shifts as shift}
+                        <ShiftCard {shift} on:sign={handleSign} />
+                    {/each}
                 </div>
-                {#each shifts as shift}
-                    <ShiftCard {shift} on:sign={handleSign}/>
-                    
-                {/each}
-            </div>
-        {/each}
+            {/each}
         {/if}
     </div>
 </div>
