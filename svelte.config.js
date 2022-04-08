4a1a5e0f0f6adb6bebfe5122703e758ec75aae1c
zithr <zither.sorrow@gmail.com>
2022-04-08 19:12:33 +0100

    add prerender

--- svelte.config.js
+++ svelte.config.js
@@ -2,7 +2,7 @@
 import adapter from "@sveltejs/adapter-static"
 import preprocess from "svelte-preprocess"
 // const baseUrl = process.env.BASE_URL || ""
-const dev = process.env.NODE_ENV === 'development';
+const dev = process.env.NODE_ENV === "development"
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -17,15 +17,16 @@ const config = {
             assets: "build",
             fallback: null,
         }),
-		paths: {
-            base: dev ? '' : '/rota-site-front',
+        paths: {
+            base: dev ? "" : "/rota-site-front",
         },
-		appDir: "internal",
-		vite: {
+        appDir: "internal",
+        vite: {
             define: {
                 "process.env": process.env,
             },
         },
+        prerender: { default: true },
 
         // hydrate the <div id="svelte"> element in src/app.html
         // target: "#svelte",
